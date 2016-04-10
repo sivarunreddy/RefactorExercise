9968957cdb35e9dfa4386b86a39954d8e084fec9
sivarunreddy <sambasiva.patti@target.com>
2016-04-10 10:03:35 +0530

    Added logic to use getLoggedUser() method

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,6 +3,7 @@ package org.craftedsw.tripservicekata.trip;
 import static org.junit.Assert.*;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,13 +14,20 @@ public class TripServiceTest {
 	
 	@Before
 	public void setup(){
-		tripService=new TripService();
+		tripService=new TestTripService();
 	}
 		
 	@Test(expected = UserNotLoggedInException.class)
 	public void test_getTripsByUser_for_user_not_loggedin_exception() throws Exception {
-		//This test case will fail due to UserSession.getLoggedUser() method
+		
 		tripService.getTripsByUser(null);
+	}
+	
+	private class TestTripService extends TripService{
+		@Override
+		protected User getLoggedUser() {
+			return null;
+		}
 	}
 	
 	
