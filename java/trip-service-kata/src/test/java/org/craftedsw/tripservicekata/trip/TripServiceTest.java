1aa0bb3716cc22111e36e3810dc60ca0b4b60b0d
sivarunreddy <sambasiva.patti@target.com>
2016-04-10 10:20:25 +0530

    Added test case to return empty friends list if the given user is not a friend of logged user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 public class TripServiceTest {
 
 	private TripService tripService;
+	private User sessionUser;
 	
 	@Before
 	public void setup(){
@@ -18,15 +19,20 @@ public class TripServiceTest {
 	}
 		
 	@Test(expected = UserNotLoggedInException.class)
-	public void test_getTripsByUser_for_user_not_loggedin_exception() throws Exception {
-		
-		tripService.getTripsByUser(null);
+	public void test_getTripsByUser_for_user_not_loggedin_exception()  {
+		tripService.getTripsByUser(new User());
+	}
+	
+	@Test()
+	public void test_getTripsByUser_to_return_empty_trip_when_not_a_friend()  {
+		sessionUser=new User();
+		tripService.getTripsByUser(new User());
 	}
 	
 	private class TestTripService extends TripService{
 		@Override
 		protected User getLoggedUser() {
-			return null;
+			return sessionUser;
 		}
 	}
 	
