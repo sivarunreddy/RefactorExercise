985f752f066b334b35bf2e30a70c376b693d2313
sivarunreddy <sambasiva.patti@target.com>
2016-04-10 09:57:14 +0530

    Added test case to validate when user is not loggedin

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,27 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.*;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class TripServiceTest {
+
+	private TripService tripService;
 	
+	@Before
+	public void setup(){
+		tripService=new TripService();
+	}
+		
+	@Test(expected = UserNotLoggedInException.class)
+	public void test_getTripsByUser_for_user_not_loggedin_exception() throws Exception {
+		//This test case will fail due to UserSession.getLoggedUser() method
+		tripService.getTripsByUser(null);
+	}
+	
+	
+
 }
